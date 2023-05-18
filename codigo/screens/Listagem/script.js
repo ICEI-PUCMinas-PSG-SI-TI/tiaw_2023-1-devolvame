8c93d4de210f277100ff47b046c180a26b1ffe41
DanLoose <danloose14@gmail.com>
2023-05-18 20:20:36 -0300

    Implementação da página de contato do objeto

--- codigo/screens/Listagem/script.js
+++ codigo/screens/Listagem/script.js
@@ -1,12 +1,22 @@
 const filterButton = document.querySelector(".filter-button");
 const filterContainer = document.querySelector(".filter-container");
+const contatoButton = document.getElementsByClassName("contato-button");
 
-function handleOpenFilterContainer(e){
-    if(filterContainer.style.display === "none" || !filterContainer.style.display) {
-        filterContainer.style.display = "flex";
-    } else {
-      filterContainer.style.display = "none";
+function handleOpenFilterContainer(e) {
+  if (filterContainer.style.display === "none" || !filterContainer.style.display) {
+    filterContainer.style.display = "flex";
+  } else {
+    filterContainer.style.display = "none";
   }
 }
 
+function handleClickContatoButton(e) {
+  window.location.href = "../ContatoObjeto/index.html"
+}
+
 filterButton.addEventListener("click", handleOpenFilterContainer)
+
+// adiciona o evento e click em todos os botoes
+for (let i = 0; i < contatoButton.length; i++) {
+  contatoButton[i].addEventListener("click", handleClickContatoButton)
+}

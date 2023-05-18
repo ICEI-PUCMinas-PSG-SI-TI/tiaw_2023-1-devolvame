888f7c13bc64ddbf685fc273f1e806ef914f7ed0
DanLoose <danloose14@gmail.com>
2023-05-18 20:56:13 -0300

    implentação de login, cadastro e listagem de user

--- codigo/global/script.js
+++ codigo/global/script.js
@@ -2,7 +2,8 @@ var objeto = []
 var usuarios = []
 
 /* cadastra admins caso localStorage vazio */
-if (!localStorage.length) {
+usuarios = JSON.parse(localStorage.getItem("usuarios"))
+if (!usuarios) {
 
     let admins = [
         { username: 'daniloloose', password: 'admin' },
@@ -12,23 +13,52 @@ if (!localStorage.length) {
         { username: 'miguelbizzi', password: 'admin' }
     ]
 
+    usuarios = []
     admins.forEach(admin => {
         usuarios.push(admin)
     })
+
+    localStorage.setItem("usuarios", JSON.stringify(usuarios))
 }
 
 /* API (CRUD) objetos */
-function cadastrarObjeto(objeto) { }
+function cadastrarObjeto() { }
 function listarObjetos() { }
-function listarObjeto(id) { }
-function filtrarObjetos(filtro) { }
-function atualizarObjeto(id) { }
-function excluirObjeto(id) { }
+function listarObjeto() { }
+function filtrarObjetos() { }
+function atualizarObjeto() { }
+function excluirObjeto() { }
 
 /* API (CRUD) usuários */
-function cadastrarUsuario(usuario) { }
-function listarUsuario(id) { }
+function cadastrarUsuario(usuario) {
+
+    // ainda não foram feitas validações, criar novo script de validação?
+    let usuarioCadastrado = listarUsuario(usuario.username)
+    if (usuarioCadastrado) return false
+
+    usuarios.push(usuario)
+    localStorage.setItem("usuarios", JSON.stringify(usuarios))
+    return usuario
+
+
+}
+function listarUsuario(username) {
+    let usuarioCadastrado = usuarios.find(usuarioCadastrado => {
+        if (username === usuarioCadastrado.username) return usuarioCadastrado
+    })
+
+    return usuarioCadastrado ? usuarioCadastrado : false
+
+}
 function listarUsuarios() { }
-function atualizarUsuario(id) { }
-function excluirUsuario(id) { }
-function loginUsuario(usuario) { }
+function atualizarUsuario(username) { }
+function excluirUsuario(username) { }
+function loginUsuario(usuario) {
+
+    let usuarioCadastrado = usuarios.find(usuarioCadastrado => {
+        if (usuario.username === usuarioCadastrado.username &&
+            usuario.password === usuarioCadastrado.password) return usuarioCadastrado
+    })
+
+    return usuarioCadastrado ? usuarioCadastrado : false
+}
